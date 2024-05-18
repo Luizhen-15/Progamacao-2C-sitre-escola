cee0e34d835afecc84fba131cb0e24af62277877
Luizhen-15 <163531160+Luizhen-15@users.noreply.github.com>
2024-05-18 00:05:46 -0300

    sdadsa

--- script.js
+++
@@ -1,7 +0,0 @@
-// Este é um exemplo básico de script JavaScript para adicionar funcionalidades ao site
-// Por exemplo, você poderia adicionar uma função para adicionar produtos ao carrinho de compras
-document.querySelectorAll('.produto button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Produto adicionado ao carrinho!');
-    });
-});
